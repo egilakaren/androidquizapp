276465b4e2f5aa407b49b2273e0a818bd6a2be52
egilakaren <egilakaren@gmail.com>
2018-05-21 15:58:07 -0300

    Questions

--- app/src/main/java/br/com/android/work/android_quizapp/Playing.java
+++ app/src/main/java/br/com/android/work/android_quizapp/Playing.java
@@ -1,5 +1,6 @@
 package br.com.android.work.android_quizapp;
 
+import android.content.Intent;
 import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,9 +9,11 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.squareup.picasso.Picasso;
 
 import br.com.android.work.android_quizapp.Common.Common;
 
@@ -62,13 +65,83 @@ public class Playing extends AppCompatActivity implements View.OnClickListener{
     }
 
     @Override
-    public void onClick(View v) {
+    public void onClick(View view) {
         mCountDown.cancel();
         if(index < totalQuestion){
             Button clickedButton = (Button) view;
-            if(clickedButton.getText().equals(Common.questionList.get(index).getCorrectAnswer())){
-                
+            if(clickedButton.getText().equals(Common.questionList.get(index).getCorrectAnswer())) {
+                score += 10;
+                correctAnswer++;
+                showQuestion(++index);
+            } else {
+                Intent intent = new Intent(this, Done.class);
+                Bundle dataSend = new Bundle();
+                dataSend.putInt("SCORE", score);
+                dataSend.putInt("TOTAL", totalQuestion);
+                dataSend.putInt("CORRECT", correctAnswer);
+                intent.putExtras(dataSend);
+                startActivity(intent);
+                finish();
             }
+            txtScore.setText(String.format("%d", score));
         }
+    }
+
+    private void showQuestion(int index) {
+
+        if(index < totalQuestion){
+            thisQuestion++;
+            txtQuestionNum.setText(String.format("%d / %d", thisQuestion, totalQuestion));
+            progressBar.setProgress(0);
+            progressValue = 0;
+
+            if(Common.questionList.get(index).getIsImageQuestion().equals("true")){
+                Picasso.with(getBaseContext()).load(Common.questionList.get(index).getQuestion()).into(question_image);
+                question_image.setVisibility(View.VISIBLE);
+                question_text.setVisibility(View.INVISIBLE);
+            } else {
+                question_text.setText(Common.questionList.get(index).getQuestion());
+                question_image.setVisibility(View.VISIBLE);
+                question_text.setVisibility(View.INVISIBLE);
+            }
+
+            btnA.setText(Common.questionList.get(index).getAnswerA());
+            btnB.setText(Common.questionList.get(index).getAnswerB());
+            btnC.setText(Common.questionList.get(index).getAnswerC());
+            btnD.setText(Common.questionList.get(index).getAnswerD());
+
+            mCountDown.start();
+        } else {
+            Intent intent = new Intent(this, Done.class);
+            Bundle dataSend = new Bundle();
+            dataSend.putInt("SCORE", score);
+            dataSend.putInt("TOTAL", totalQuestion);
+            dataSend.putInt("CORRECT", correctAnswer);
+            intent.putExtras(dataSend);
+            startActivity(intent);
+            finish();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        totalQuestion = Common.questionList.size();
+
+        mCountDown = new CountDownTimer(TIMEOUT, INTERVAL) {
+            @Override
+            public void onTick(long minisec) {
+                progressBar.setProgress(progressValue);
+                progressValue++;
+            }
+
+            @Override
+            public void onFinish() {
+                mCountDown.cancel();
+                showQuestion(++index);
+            }
+        };
+        showQuestion(++index);
     }
 }
