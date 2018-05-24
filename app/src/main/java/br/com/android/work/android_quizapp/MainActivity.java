60a7a5a43432b08405cdebac6e30b798e81f262a
Egila Karen <egilakaren@gmail.com>
2018-05-24 11:07:42 -0300

    Update MainActivity.java

--- app/src/main/java/br/com/android/work/android_quizapp/MainActivity.java
+++ app/src/main/java/br/com/android/work/android_quizapp/MainActivity.java
@@ -94,7 +94,7 @@ public class MainActivity extends AppCompatActivity {
 
     private void showSignUpDialog() {
         AlertDialog.Builder alertDialog = new AlertDialog.Builder(MainActivity.this);
-        alertDialog.setTitle("Sign Up");
+        alertDialog.setTitle("Cadastro");
         alertDialog.setMessage("Por favcr, preencha todos os campos");
 
         LayoutInflater inflater = this.getLayoutInflater();
